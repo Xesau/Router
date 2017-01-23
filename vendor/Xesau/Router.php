6722a4e8e5d830fdff8bb04481e461e83c961d55
Xesau <Xesau@users.noreply.github.com>
2017-01-23 22:00:48 +0100

    Simplified ->route, added docs, added mounting...
    and fixed some spacing.

--- vendor/Xesau/Router.php
+++ vendor/Xesau/Router.php
@@ -13,18 +13,28 @@ class Router
 {
 
     /**
-     * @var array[][] $routes The defined routes
-     * @var callback  $error  The error handler, invoked as ($method, $path)
+     * @var array[][] $routes        The defined routes
+     * @var callback  $error         The error handler, invoked as ($method, $path)
+     * @var string    $baseNamespace The base namespace
+     * @var string    $currentPrefix The current route prefix
      */
     private $routes;
     private $error;
     private $baseNamespace;
-
+    private $currentPrefix;
+    
+    /**
+     * Initiates the router and sets some default values
+     *
+     * @param callable $error         The 404 Not Found handler
+     * @param string   $baseNamespace The base namespace
+     */
     public function __construct($error, $baseNamespace = '')
     {
         $this->routes = [];
         $this->error = $error;
         $this->baseNamespace = $baseNamespace == '' ? '' : $baseNamespace.'\\';
+        $this->currentPrefix = '';
     }
 
     /**
@@ -35,19 +45,36 @@ class Router
      */
     public function route($method, $regex, $handler)
     {
-        if (!is_array($method)) {
-            if ($method == '*')
-                foreach (['GET', 'PUT', 'DELETE', 'OPTIONS', 'TRACE', 'POST', 'HEAD'] as $method)
-                    $this->routes[$method][$regex] = $handler;
-            else
-                $this->routes[strtoupper($method)][$regex] = $handler;
-        } else {
-            foreach ($method as $m)
-                $this->routes[strtoupper($m)][$regex] = $handler;
+        if ($method == '*') {
+            $method = ['GET', 'PUT', 'DELETE', 'OPTIONS', 'TRACE', 'POST', 'HEAD'];
         }
+        
+        foreach ((array)$method as $m) {
+            $this->addRoute($m, $regex, $handler);
+        }
+        
         return $this;
     }
+    
+    private function addRoute($method, $regex, $handler) {
+        $this->routes[strtoupper($method)][$this->currentPrefix . $regex] = $handler;
+    }
 
+    /**
+     * Prefix a group of routes
+     *
+     * @param string $prefix The prefix
+     * @param callable $routes callable(Router) wherein the mounted routes be added
+     */
+    public function mount($prefix, callable $routes) {
+        $previous = $this->currentPrefix;
+        $this->currentPrefix = $prefix;
+        $routes($this);
+        $this->currentPrefix = $previous;
+        
+        return $this;
+    }
+    
     /**
      * Adds a route to the GET route collection
      *
@@ -56,7 +83,7 @@ class Router
      */
     public function get($regex, $handler)
     {
-        $this->routes['GET'][$regex] = $handler;
+        $this->addRoute('GET', $regex, $handler);
         return $this;
     }
 
@@ -68,7 +95,7 @@ class Router
      */
     public function post($regex, $handler)
     {
-        $this->routes['POST'][$regex] = $handler;
+        $this->addRoute('POST', $regex, $handler);
         return $this;
     }
 
@@ -80,7 +107,7 @@ class Router
      */
     public function put($regex, $handler)
     {
-        $this->routes['PUT'][$regex] = $handler;
+        $this->addRoute('PUT', $regex, $handler);
         return $this;
     }
 
@@ -92,7 +119,7 @@ class Router
      */
     public function head($regex, $handler)
     {
-        $this->routes['HEAD'][$regex] = $handler;
+        $this->addRoute('HEAD', $regex, $handler);
         return $this;
     }
 
@@ -104,31 +131,43 @@ class Router
      */
     public function delete($regex, $handler)
     {
-        $this->routes['DELETE'][$regex] = $handler;
+        $this->addRoute('DELETE', $regex, $handler);
         return $this;
     }
     
     /**
-     * Adds a route to the DELETE route collection
+     * Adds a route to the OPTIONS route collection
      *
      * @param string $regex    The path, allowing regex
      * @param string $callable The handler
      */
     public function options($regex, $handler)
     {
-        $this->routes['options'][$regex] = $handler;
+        $this->addRoute('OPTIONS', $regex, $handler);
         return $this;
     }
 
     /**
-     * Adds a route to the DELETE route collection
+     * Adds a route to the TRACE route collection
      *
      * @param string $regex    The path, allowing regex
      * @param string $callable The handler
      */
     public function trace($regex, $handler)
     {
-        $this->routes['trace'][$regex] = $handler;
+        $this->addRoute('TRACE', $regex, $handler);
+        return $this;
+    }
+
+    /**
+     * Adds a route to the CONNECT route collection
+     *
+     * @param string $regex    The path, allowing regex
+     * @param string $callable The handler
+     */
+    public function connect($regex, $handler)
+    {
+        $this->addRoute('CONNECT', $regex, $handler);
         return $this;
     }
 
@@ -169,32 +208,39 @@ class Router
         // Nothing found --> error handler
         return $this->call($this->error, [$method, $path]);
     }
-	
-	private function call($callable, array $params = []) {
-		if (is_string($callable)) {
-			if (strlen($callable) > 0) {
-				if ($callable[0] == '@') {
-					$callable = $this->baseNamespace . substr($callable, 1);
-				}
-			} else {
-				throw new InvalidArgumentException('Route/error callable as string must not be empty.');
-			}
-			$callable = str_replace('.', '\\', $callable);
-		}
-		if (is_array($callable)) {
-			if (count($callable) !== 2)
-				throw new InvalidArgumentException('Route/error callable as array must contain and contain only two strings.');
-			if (strlen($callable[0]) > 0) {
-				if ($callable[0][0] == '@') {
-					$callable[0] = $this->baseNamespace . substr($callable[0], 1);
-				}
-			} else {
-				throw new InvalidArgumentException('Route/error callable as array must contain and contain only two strings.');
-			}
-			$callable[0] = str_replace('.', '\\', $callable[0]);
-		}
-		return call_user_func_array($callable, $params);
-	}
+    
+    /**
+     * Internal function to parse and call custom callables
+     * 
+     * @param mixed $callable string, string[] or callable to call
+     * @param array $params   The parameters to send to call_user_func_array
+     * @return mixed The results from the call
+     */
+    private function call($callable, array $params = []) {
+        if (is_string($callable)) {
+            if (strlen($callable) > 0) {
+                if ($callable[0] == '@') {
+                    $callable = $this->baseNamespace . substr($callable, 1);
+                }
+            } else {
+                throw new InvalidArgumentException('Route/error callable as string must not be empty.');
+            }
+            $callable = str_replace('.', '\\', $callable);
+        }
+        if (is_array($callable)) {
+            if (count($callable) !== 2)
+                throw new InvalidArgumentException('Route/error callable as array must contain and contain only two strings.');
+            if (strlen($callable[0]) > 0) {
+                if ($callable[0][0] == '@') {
+                    $callable[0] = $this->baseNamespace . substr($callable[0], 1);
+                }
+            } else {
+                throw new InvalidArgumentException('Route/error callable as array must contain and contain only two strings.');
+            }
+            $callable[0] = str_replace('.', '\\', $callable[0]);
+        }
+        return call_user_func_array($callable, $params);
+    }
 
     /**
      * Dispatches the router using data from the $_SERVER global
@@ -204,7 +250,16 @@ class Router
     public function dispatchGlobal()
     {
         $pos = strpos($_SERVER['REQUEST_URI'], '?');
-        return $this->dispatch($_SERVER['REQUEST_METHOD'], '/' . trim(substr($pos !== false ? substr($_SERVER['REQUEST_URI'], 0, $pos) : $_SERVER['REQUEST_URI'], strlen(implode('/', array_slice(explode('/', $_SERVER['SCRIPT_NAME']), 0, -1)) . '/')), '/'));
+        return $this->dispatch(
+            $_SERVER['REQUEST_METHOD'],
+            '/'. trim(
+                substr($pos !== false
+                ?   substr($_SERVER['REQUEST_URI'], 0, $pos)
+                :   $_SERVER['REQUEST_URI'],
+                strlen(implode('/', array_slice(explode('/', $_SERVER['SCRIPT_NAME']), 0, -1)) .'/')),
+                '/'
+            )
+        );
     }
 
 }
